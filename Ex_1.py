0dbb20eb0dc32fcc6d5aee7d138e4fa4cb58eeac
evgueniaz <evgueniaz@hotmail.com>
2021-11-01 02:06:15 +0300

    HW3

---
+++ Ex_1.py
@@ -0,0 +1,10 @@
+
+numbers = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
+               'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
+def num_translate(num):
+    try:
+        return print(numbers[num])
+    except:
+        return print('None')
+
+num_translate('five')
---
+++ Ex_2.py
@@ -0,0 +1,22 @@
+
+
+numbers = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
+               'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
+def num_translate_adv(num):
+    if num.istitle() is False:
+        try:
+            return print(numbers[num])
+        except:
+            return print('None')
+    else:
+        cap_num = num.lower()
+        try:
+            return print(numbers[cap_num].capitalize())
+        except:
+            return print('None')
+
+num_translate_adv('Five')
+num_translate_adv('ten')
+num_translate_adv('red')
+num_translate_adv('Sad')
+num_translate_adv('Eight')
