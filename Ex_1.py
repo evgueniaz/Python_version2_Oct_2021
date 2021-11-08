82cf88e3438330f672a4cbed99164dc179905613
evgueniaz <85039212+evgueniaz@users.noreply.github.com>
2021-11-08 16:30:55 +0300

    Update Ex_1.py

--- Ex_1.py
+++ Ex_1.py
@@ -1,7 +1,8 @@
 
-numbers = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
-               'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
+
 def num_translate(num):
+    numbers = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
+               'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
     try:
         return print(numbers[num])
     except:
