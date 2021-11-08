05e9779210f4644e77312afb1bde9d274433a93e
evgueniaz <85039212+evgueniaz@users.noreply.github.com>
2021-11-08 16:32:16 +0300

    Update Ex_2.py

--- Ex_2.py
+++ Ex_2.py
@@ -1,8 +1,9 @@
 
 
-numbers = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
-               'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
+
 def num_translate_adv(num):
+    numbers = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
+               'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
     if num.istitle() is False:
         try:
             return print(numbers[num])
